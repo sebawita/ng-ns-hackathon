98466b0789218bb60ea53fc11d3477537a554191
sebawita <sebawita@gmail.com>
2019-08-14 19:06:53 +0100

    Add nav-bar menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 
 // Material Design Components
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatCardModule } from '@angular/material/card';
+import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
 
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
-import { MatListModule } from '@angular/material/list';
+
 
 import { IntroComponent } from './intro/intro.component';
 import { KeyFactsComponent } from './key-facts/key-facts.component';
@@ -50,12 +50,11 @@ import { PrizesComponent } from './prizes/prizes.component';
     AppRoutingModule,
     BrowserAnimationsModule,
     // Material Design Components
-    MatToolbarModule,
     MatButtonModule,
     MatListModule,
     MatIconModule,
-    MatTooltipModule,
     MatCardModule,
+    MatMenuModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
