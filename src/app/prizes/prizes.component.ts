fc71b14b8e1ca077c5e777e73fffa5ff3fbd88f8
sebawita <sebawita@gmail.com>
2019-08-21 21:06:31 +0200

    update prizes

--- src/app/prizes/prizes.component.ts
+++ src/app/prizes/prizes.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 interface Category {
   name: string;
   description: string;
-  prizes: Prize[];
+  prize: string;
 }
 
 interface Prize {
@@ -17,42 +17,34 @@ interface Prize {
   styleUrls: ['./prizes.component.scss']
 })
 export class PrizesComponent {
-  categories: Category[] = [
+  mainCategories: Category[] = [
     {
       name: 'Judges Favorite',
       description: 'Score will be awarded by the judges and calculated based on Innovation, Design and Utility.',
-      prizes: [
-        { name: 'Winner', prize: '2000 USD' },
-        { name: '2x Honorary Mention', prize: '500 USD' },
-      ]
+      prize: '1500 USD',
     },
     {
       name: 'Hacker Favorite',
       description: 'Popularity score will be calculated based on the number of votes an entry gets from other participants.',
-      prizes: [
-        { name: '3x Winner', prize: '500 USD' },
-      ]
+      prize: '1000 USD',
     },
+  ];
+
+  subcategories: Category[] = [
     {
       name: 'Design Winner',
       description: 'Best score for category of "Design"',
-      prizes: [
-        { name: 'Winner', prize: '500 USD' },
-      ]
+      prize: '500 USD',
     },
     {
       name: 'Innovation Winner',
       description: 'Best score for category of "Innovation"',
-      prizes: [
-        { name: 'Winner', prize: '500 USD' },
-      ]
+      prize: '500 USD',
     },
     {
       name: 'Utility Winner',
       description: 'Best score for category of "Utility"',
-      prizes: [
-        { name: 'Winner', prize: '500 USD' },
-      ]
+      prize: '500 USD',
     },
   ];
 }
